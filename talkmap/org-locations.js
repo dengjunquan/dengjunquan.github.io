bcf0928d8d63fd36cfe993546f479202aeb8ca9b
dengjunquan <804734792@qq.com>
2018-08-04 00:27:04 +0300

    locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,7 +1,28 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "San Diego, USA",
+    32.71533, 
+    -117.15726
+  ],
+
+  [
+    "Oulu, Finland",
+    65.01236, 
+    25.46816
+  ],
+
+  [ "Paris, France",
+    48.85341, 
+    2.3488
+  ],
+
+  [ "Barcelona, Spain",
+  41.38879, 
+  2.15899
+  ],
+
+  [ "Valencia, Spain",
+  39.46975, 
+  -0.37739
   ],
 ];
